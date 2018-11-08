bd2ec1750468d0c578ed878f673ba445b8940d27
alivls <alivls@yandex.ru>
2018-11-08 12:41:25 +0500

    Update preloader/js/script.js
    Перепутал setInterval  и setTimeout

--- preloader/js/script.js
+++ preloader/js/script.js
@@ -1,16 +1,17 @@
 function preloader() {
     $(() =>{
 
-        setInterval(() =>{
+        setTimeout(() =>{
 
             let p = $('.preloader');
             p.css('opacity', 0);
-            setInterval(
+            setTimeout(
                 ()=> p.remove(),
                 parseInt(p.css('1s'))* 1000
+                
             );
 
-        }, 1000);
+            }, 1000);
 
     });
 }
